b2b976433ae4ff27a6d6ff8d0eea3e4c3973a46c
Ahmed Baig <ahmedbaig137@gmail.com>
2024-03-30 20:55:21 +0500

    Update mlops_A1_model.py

--- mlops_A1_model.py
+++ mlops_A1_model.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-#added code
 
 def main():
     # Load the iris dataset
